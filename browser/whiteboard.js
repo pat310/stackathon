dfa01ef9170c3d774db1d29ecb881c6dd69db930
Patrick Trasborg <patrick.trasborg@gmail.com>
2015-08-06 16:28:21 -0400

    able to draw with phone

--- browser/whiteboard.js
+++ browser/whiteboard.js
@@ -2,58 +2,6 @@ window.whiteboard = new window.EventEmitter();
 
 (function () {
 
-    var x = 0, y = 0,
-    vx = 0, vy = 0,
-    ax = 0, ay = 0;
-        
-    var sphere = document.getElementById("sphere");
-
-    if (window.DeviceMotionEvent != undefined) {
-        window.ondevicemotion = function(e) {
-            ax = event.accelerationIncludingGravity.x * 5;
-            ay = event.accelerationIncludingGravity.y * 5;
-            document.getElementById("accelerationX").innerHTML = e.accelerationIncludingGravity.x;
-            document.getElementById("accelerationY").innerHTML = e.accelerationIncludingGravity.y;
-            document.getElementById("accelerationZ").innerHTML = e.accelerationIncludingGravity.z;
-
-            if ( e.rotationRate ) {
-                document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
-                document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
-                document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
-            }       
-        }
-
-        setInterval( function() {
-            var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
-            if ( landscapeOrientation) {
-                vx = vx + ay;
-                vy = vy + ax;
-            } else {
-                vy = vy - ay;
-                vx = vx + ax;
-            }
-            vx = vx * 0.98;
-            vy = vy * 0.98;
-            y = parseInt(y + vy / 50);
-            x = parseInt(x + vx / 50);
-            
-            boundingBoxCheck();
-            
-            sphere.style.top = y + "px";
-            sphere.style.left = x + "px";
-            
-        }, 25);
-    } 
-
-
-    function boundingBoxCheck(){
-        if (x<0) { x = 0; vx = -vx; }
-        if (y<0) { y = 0; vy = -vy; }
-        if (x>document.documentElement.clientWidth-20) { x = document.documentElement.clientWidth-20; vx = -vx; }
-        if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = -vy; }
-        
-    }
-
     // Ultimately, the color of our stroke;
     var color;
 
@@ -124,6 +72,76 @@ window.whiteboard = new window.EventEmitter();
 
         whiteboard.draw(lastMousePosition, currentMousePosition, color, true);
 
+    });
+
+
+//ball stuff
+    var x = 0, y = 0,
+    vx = 0, vy = 0,
+    ax = 0, ay = 0,
+    xlast = 0, ylast = 0;
+    
+    var sphere = document.getElementById("sphere");
+
+    if (window.DeviceMotionEvent !== undefined) {
+
+        window.ondevicemotion = function(e) {
+            axlast = ax;
+            aylast = ay;
+            ax = e.accelerationIncludingGravity.x * 5;
+            ay = e.accelerationIncludingGravity.y * 5;
+        };
+
+        setInterval( function() {
+            var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
+            if ( landscapeOrientation) {
+                vx = vx + ay;
+                vy = vy + ax;
+            } else {
+                vy = vy - ay;
+                vx = vx + ax;
+            }
+            vx = vx * 0.98;
+            vy = vy * 0.98;
+            
+            xlast = x;
+            ylast = y;
+
+            y = parseInt(y + vy / 400);
+            x = parseInt(x + vx / 400);
+            
+            boundingBoxCheck();
+            
+            sphere.style.top = y + "px";
+            sphere.style.left = x + "px";
+            
+        }, 25);
+    } 
+
+
+    function boundingBoxCheck(){
+        if (x<0) { x = 0; vx = -vx; }
+        if (y<0) { y = 0; vy = -vy; }
+        if (x>document.documentElement.clientWidth-20) { x = document.documentElement.clientWidth-20; vx = -vx; }
+        if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = -vy; }
+        
+    }
+
+//end of ball stuff*****************************************
+
+    var scaleFactorx = 4.5;
+    var scaleFactory = 1.5;
+
+    var current = {x:0, y:0};
+    var last = {x:0, y:0};
+
+    window.addEventListener('devicemotion', function(e){
+        last.x = x*scaleFactorx;
+        last.y = y*scaleFactory;    
+        current.x = parseInt(xlast)*scaleFactorx;
+        current.y = parseInt(ylast)*scaleFactory;
+
+        whiteboard.draw(last, current, color, true);
     });
 
     whiteboard.draw = function (start, end, strokeColor, shouldBroadcast) {
